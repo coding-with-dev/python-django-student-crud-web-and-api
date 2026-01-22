0027fcd06ab5f021de7867a7a6dd081d45fe1761
coding-with-dev <codewithdevendra@gmail.com>
2026-01-22 23:58:53 +0530

    Student CRUD - Completed Edit, Details and Delete Web Page

--- students/urls.py
+++ students/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.list_students, name="list_students"),
     path('add/', views.add_student, name="add_student"),
-    path('add_model_base/', views.add_student_model_form, name="add_student_model_form"),
-    path('edit/<int:pk>', views.edit_student, name="edit_student"),
-    path('details/<int:pk>', views.details_student, name="details_student")
+    path('edit/<int:id>', views.edit_student, name="edit_student"),
+    path('details/<int:id>', views.details_student, name="details_student"),
+    path('delete/<int:id>', views.delete_student, name="delete_student")
 ]
--- students/views.py
+++ students/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 
 from .models import Students
@@ -13,63 +13,34 @@ def list_students(request):
 
 def add_student(request):
     if request.method == "POST":
-        validation_errors = []
-        success_message = ''
-
-        fullname = request.POST.get('fullname')
-        email_addr = request.POST.get('email_addr')
-        mobile_no = request.POST.get('mobile_no')
-        age = request.POST.get('age')
-        photo = request.FILES.get('photo')
-
-        if not fullname: 
-            validation_errors.append('Please enter fullname')
-
-        if not email_addr: 
-            validation_errors.append('Please enter email address')
-
-        if not mobile_no: 
-            validation_errors.append('Please enter mobile no')
-
-        if not age: 
-            validation_errors.append('Please enter age')
-
-        if not validation_errors:
-            Students.objects.create(
-                name = fullname, 
-                email = email_addr,
-                age = age,
-                mobile = mobile_no,
-                photo = photo
-            )
-
-        if not validation_errors:
-            success_message = "Student details added successfully"
-            request.POST = []
-
-        page_data = {
-            "errors": validation_errors,
-            "success": success_message
-        }
-        return render(request, 'students/add.html', page_data)
-    else:    
-        return render(request, 'students/add.html')
-
-def edit_student(request):
-    return render(request, 'students/edit.html')
-
-def details_student(request):
-    return render(request, 'students/details.html')
-
-def add_student_model_form(request):
-    if request.method == "POST":
         form = StudentForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             messages.success(request, "Student added successfully!")
-            return redirect(request.path)
-        
+            return redirect(request.path)        
     else:
         form = StudentForm()
     
-    return render(request, 'students/add-model-base.html', {'form': form})
+    return render(request, 'students/add.html', {'form': form})
+
+def edit_student(request, id):
+    student = get_object_or_404(Students, id=id)
+    if request.method == "POST":
+        form = StudentForm(request.POST, request.FILES, instance=student)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Student added successfully!")
+            return redirect(request.path)        
+    else:
+        form = StudentForm(instance=student)
+
+    return render(request, 'students/edit.html', {'form': form, 'student': student})
+
+def details_student(request, id):
+    student = get_object_or_404(Students, id=id)
+    return render(request, 'students/details.html', {'student': student})
+
+def delete_student(request, id):
+    student = get_object_or_404(Students, id=id)
+    student.delete()
+    return redirect('list_students')
